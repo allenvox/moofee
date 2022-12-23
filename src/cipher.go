611a39cd16097385698d1c6de744170e9aaf4a00
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-23 17:53:34 +0700

    Implement Vigenere cipher

--- src/cipher.go
+++ src/cipher.go
@@ -7,8 +7,7 @@ import (
 
 func caesar(text string, shift int) string {
 	shift = (shift%33 + 33) % 33 // only between 0 and 32
-	text = strings.ToLower(text)
-	chars := []rune(text)
+	chars := []rune(strings.ToLower(text))
 	for i := 0; i < len(chars); i++ {
 		if !unicode.IsLetter(chars[i]) {
 			continue // skip
@@ -20,4 +19,24 @@ func caesar(text string, shift int) string {
 		chars[i] = char
 	}
 	return string(chars)
+}
+
+func vigenere(text string, key string) string {
+	textChars := []rune(strings.ToLower(text))
+	keyChars := []rune(key)
+	keylen := len(keyChars)
+	letter := 0
+	for i := 0; i < len(textChars); i++ {
+		if !unicode.IsLetter(textChars[i]) {
+			continue
+		}
+		letter %= keylen
+		char := textChars[i] + keyChars[letter] - 'а'
+		if char > 'я' {
+			char = char - 'я' + 'а'
+		}
+		textChars[i] = char
+		letter++
+	}
+	return string(textChars)
 }
