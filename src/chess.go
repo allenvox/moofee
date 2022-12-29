45d326bc1c03ebc3367bf9b388bce31edb0be85e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-29 16:08:36 +0700

    Add puzzle description getting

--- src/chess.go
+++ src/chess.go
@@ -1,5 +1,24 @@
 package main
 
-func getPuzzle(number int) {
+import "strings"
 
+func puzzleDescription(name string, flag *int) string {
+	var result string = "Задача — поставить мат за "
+	if strings.HasPrefix(name, "m2") {
+		result += "2"
+	} else {
+		result += "3"
+	}
+	result += " хода\n\n"
+	switch name {
+	case "m2_1":
+		*flag = m2_1
+	case "m2_2":
+		*flag = m2_2
+	case "m3_1":
+		*flag = m3_1
+	case "m3_2":
+		*flag = m3_2
+	}
+	return result + "Формат хода (в сообщении): прошлое_место_фигуры-новое_место_фигуры\nПример: e2-e4"
 }
