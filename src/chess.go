422d8a35177ee07ce26086ca9ddbebe83a042858
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-01 13:59:34 +0700

    Rework puzzle flags and solutions handling

--- src/chess.go
+++ src/chess.go
@@ -1,68 +1,62 @@
 package main
 
 import (
-	"strings"
+	"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var chess_puzzle_number = 0
+
 func puzzleDescription(name string, flag *int) string {
-	var result string = "Задача — поставить мат за "
-	if strings.HasPrefix(name, "m2") {
-		result += "2"
-	} else {
-		result += "3"
+	var result string = "Задача — поставить мат за " + string([]rune(name)[1]) + " ход"
+	if *flag >= m2 {
+		result += "а"
 	}
-	result += " хода\n\n"
-	switch name {
-	case "m2_1":
-		*flag = m2_1
-	case "m2_2":
-		*flag = m2_2
-	case "m3_1":
-		*flag = m3_1
-	case "m3_2":
-		*flag = m3_2
-	}
+	result += "\n\n"
+	chess_puzzle_number, _ = strconv.Atoi(string([]rune(name)[3]))
 	return result + "Формат хода (в сообщении): прошлое_место_фигуры-новое_место_фигуры\nПример: e2-e4"
 }
 
 var move int = 1
-var chess_phrases = []string{"❌ Неверный ход\nПопробуйте другой", "✅ Отличное начало. Введите второй ход", "✅ Замечательно! Введите третий ход", "🎂 Поздравляю!\n✅ Вы выполнили шахматную задачу ♟"}
-var solution_m2_1 = []string{"b5-a5", "a5-a1"}
-var solution_m2_2 = []string{"f2-f8", "f8-e8"}
-var solution_m3_1 = []string{"f8-h8", "d8-d6", "h8-f8"}
-var solution_m3_2 = []string{"c8-g4", "g4-d1", "d1-e1"}
+var chess_phrases = []string{"❌ Неверный ход\nПопробуйте другой", "✅ Отличное начало. Введите второй ход", "✅ Замечательно! Введите третий ход", "🎂 Поздравляю!\n✅ Вы выполнили шахматную задачу ♟", "✅ Третий ход верный! Введите четвёртый ход"}
+var chess_solutions = [][][]string{
+	{ // mate in 1
+
+	},
+
+	{ // m2
+		{"b5-a5", "a5-a1"},
+		{"f2-f8", "f8-e8"},
+	},
+
+	{ // m3
+		{"f8-h8", "d8-d6", "h8-f8"},
+		{"c8-g4", "g4-d1", "d1-e1"},
+	},
+
+	{ //m4
+
+	},
+}
 
 func handlePuzzle(update tgbotapi.Update, flag *int) string {
 	text := update.Message.Text
 	var result string = "std"
-
-	var move_solution = []string{}
-	switch *flag {
-	case m2_1:
-		move_solution = solution_m2_1
-	case m2_2:
-		move_solution = solution_m2_2
-	case m3_1:
-		move_solution = solution_m3_1
-	case m3_2:
-		move_solution = solution_m3_2
-	}
-
+	move_solution := chess_solutions[*flag-5][chess_puzzle_number-1][move-1]
 	switch move {
 	case 1:
-		if text != move_solution[0] {
+		if text != move_solution {
 			result = chess_phrases[0]
 		} else {
 			result = chess_phrases[1]
 			move++
 		}
 	case 2:
-		if text != move_solution[1] {
+		if text != move_solution {
 			result = chess_phrases[0]
 		} else {
-			if *flag < m3_1 {
+			if *flag < m3 {
 				result = chess_phrases[3]
 				*flag = no_flag
 				move--
@@ -72,7 +66,20 @@ func handlePuzzle(update tgbotapi.Update, flag *int) string {
 			}
 		}
 	case 3:
-		if text != move_solution[2] {
+		if text != move_solution {
+			result = chess_phrases[0]
+		} else {
+			if *flag < m4 {
+				result = chess_phrases[3]
+				*flag = no_flag
+				move = 1
+			} else {
+				result = chess_phrases[4]
+				move++
+			}
+		}
+	case 4:
+		if text != move_solution {
 			result = chess_phrases[0]
 		} else {
 			result = chess_phrases[3]
