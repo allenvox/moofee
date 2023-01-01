68b7552a123960e8a902f92350c5a36188b536ab
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-01 13:40:43 +0700

    Fix flags

--- src/main.go
+++ src/main.go
@@ -1,20 +1,5 @@
 package main
 
-const (
-	no_flag = iota
-	caesar_phrase
-	caesar_shift
-	vigenere_phrase
-	vigenere_key
-	quest
-	m2_1
-	m2_2
-	m3_1
-	m3_2
-	m4_1
-	m4_2
-)
-
 func main() {
 	bot := initBot()
 	updates := getUpdates(bot)
