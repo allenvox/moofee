19a1c51e810cd52616bbc645f5b39fdb148c7139
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-08 13:35:34 +0700

    Add localisation for keyboards

---
+++ src/locales.go
@@ -0,0 +1,25 @@
+package main
+
+// array[0] - english, array[1] - russian
+
+var ( // keyboards
+	cipher_locale        = []string{"Ciphers", "Шифры"}
+	chords_locale        = []string{"Chords", "Аккорды"}
+	chess_locale         = []string{"Chess", "Шахматы"}
+	help_locale          = []string{"Help", "Помощь"}
+	author_locale        = []string{"Author", "Автор"}
+	version_locale       = []string{"Version", "Версия"}
+	time_locale          = []string{"Time", "Время"}
+	date_locale          = []string{"Date", "Дата"}
+	back_locale          = []string{"Back", "Назад"}
+	caesar_locale        = []string{"Caesar", "Цезарь"}
+	vigenere_locale      = []string{"Vigenere", "Виженер"}
+	encode_locale        = []string{"Encode", "Зашифровать"}
+	decode_locale        = []string{"Decode", "Расшифровать"}
+	artists_locale       = []string{"Artists", "Исполнители"}
+	other_locale         = []string{"Other", "Другое"}
+	chess_puzzles_locale = []string{"Chess puzzles", "Шахматные задачи"}
+	mate_locale          = []string{"Mate in", "Мат в"}
+	mate_in_1_locale     = []string{"", "ход"}
+	move_locale          = []string{"", "хода"}
+)
