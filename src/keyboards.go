274f3167d3cbe28a8ba3ec16b6946415c0a4da49
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-22 11:57:16 +0700

    Made english/russian variants of keyboards

--- src/keyboards.go
+++ src/keyboards.go
@@ -4,176 +4,359 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var start_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("🔐 "+cipher_locale[language], "code"),
-		tgbotapi.NewInlineKeyboardButtonData("🎸 "+chords_locale[language], "chords"),
+var start_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🔐 "+cipher_locale[0], "code"),
+			tgbotapi.NewInlineKeyboardButtonData("🎸 "+chords_locale[0], "chords"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("♟️ "+chess_locale[0], "chess"),
+			tgbotapi.NewInlineKeyboardButtonData("🆘 "+help_locale[0], "help"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("♟️ "+chess_locale[language], "chess"),
-		tgbotapi.NewInlineKeyboardButtonData("🆘 "+help_locale[language], "help"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🔐 "+cipher_locale[1], "code"),
+			tgbotapi.NewInlineKeyboardButtonData("🎸 "+chords_locale[1], "chords"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("♟️ "+chess_locale[1], "chess"),
+			tgbotapi.NewInlineKeyboardButtonData("🆘 "+help_locale[1], "help"),
+		),
 	),
-)
+}
 
 var current_language string
 
-var help_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("✍ "+author_locale[language], "author"),
-		tgbotapi.NewInlineKeyboardButtonData("1️⃣0️⃣1️⃣ "+version_locale[language], "version"),
+var help_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("✍ "+author_locale[0], "author"),
+			tgbotapi.NewInlineKeyboardButtonData("1️⃣0️⃣1️⃣ "+version_locale[0], "version"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🕦 "+time_locale[0], "time"),
+			tgbotapi.NewInlineKeyboardButtonData("📅 "+date_locale[0], "date"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(current_language+" "+language_locale[0], "language"),
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[0], "start"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("🕦 "+time_locale[language], "time"),
-		tgbotapi.NewInlineKeyboardButtonData("📅 "+date_locale[language], "date"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("✍ "+author_locale[1], "author"),
+			tgbotapi.NewInlineKeyboardButtonData("1️⃣0️⃣1️⃣ "+version_locale[1], "version"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🕦 "+time_locale[1], "time"),
+			tgbotapi.NewInlineKeyboardButtonData("📅 "+date_locale[1], "date"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(current_language+" "+language_locale[1], "language"),
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[1], "start"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData(current_language+" "+language_locale[language], "language"),
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[language], "start"),
-	),
-)
+}
 
-var code_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData(caesar_locale[language], "caesar"),
-		tgbotapi.NewInlineKeyboardButtonData(vigenere_locale[language], "vigenere"),
+var code_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(caesar_locale[0], "caesar"),
+			tgbotapi.NewInlineKeyboardButtonData(vigenere_locale[0], "vigenere"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[0], "start"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[language], "start"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(caesar_locale[1], "caesar"),
+			tgbotapi.NewInlineKeyboardButtonData(vigenere_locale[1], "vigenere"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[1], "start"),
+		),
 	),
-)
+}
 
-var vigenere_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData(encode_locale[language], "vigenere_encode"),
-		tgbotapi.NewInlineKeyboardButtonData(decode_locale[language], "vigenere_decode"),
+var vigenere_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(encode_locale[0], "vigenere_encode"),
+			tgbotapi.NewInlineKeyboardButtonData(decode_locale[0], "vigenere_decode"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+cipher_locale[0], "code"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+cipher_locale[language], "code"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(encode_locale[1], "vigenere_encode"),
+			tgbotapi.NewInlineKeyboardButtonData(decode_locale[1], "vigenere_decode"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+cipher_locale[1], "code"),
+		),
 	),
-)
+}
 
-var chess_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[language]+" 1 "+move_locale[language], "mate_in1"),
-		tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[language]+" 2 "+moves_locale[language], "mate_in2"),
+var chess_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[0]+" 1 "+move_locale[0], "mate_in1"),
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[0]+" 2 "+moves_locale[0], "mate_in2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[0]+" 3 "+moves_locale[0], "mate_in3"),
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[0]+" 4 "+moves_locale[0], "mate_in4"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_locale[0], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[language]+" 3 "+moves_locale[language], "mate_in3"),
-		tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[language]+" 4 "+moves_locale[language], "mate_in4"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[1]+" 1 "+move_locale[1], "mate_in1"),
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[1]+" 2 "+moves_locale[1], "mate_in2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[1]+" 3 "+moves_locale[1], "mate_in3"),
+			tgbotapi.NewInlineKeyboardButtonData("🧩 "+mate_locale[1]+" 4 "+moves_locale[1], "mate_in4"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_locale[1], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_locale[language], "chess"),
-	),
-)
+}
 
-var mate_in1_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("1", "m1_1"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "m1_2"),
+var mate_in1_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m1_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m1_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[0], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[language], "chess"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m1_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m1_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[1], "chess"),
+		),
 	),
-)
+}
 
-var mate_in2_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("1", "m2_1"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "m2_2"),
+var mate_in2_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m2_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m2_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[0], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[language], "chess"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m2_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m2_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[1], "chess"),
+		),
 	),
-)
+}
 
-var mate_in3_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("1", "m3_1"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "m3_2"),
+var mate_in3_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m3_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m3_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[0], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[language], "chess"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m3_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m3_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[1], "chess"),
+		),
 	),
-)
+}
 
-var mate_in4_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("1", "m4_1"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "m4_2"),
+var mate_in4_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m4_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m4_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[0], "chess"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[language], "chess"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("1", "m4_1"),
+			tgbotapi.NewInlineKeyboardButtonData("2", "m4_2"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+chess_puzzles_locale[1], "chess"),
+		),
 	),
-)
+}
 
-var chords_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Стрыкало", "strykalo"),
-		tgbotapi.NewInlineKeyboardButtonData("Нервы", "nervy"),
+var chords_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Стрыкало", "strykalo"),
+			tgbotapi.NewInlineKeyboardButtonData("Нервы", "nervy"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("кис-кис", "kiskis"),
+			tgbotapi.NewInlineKeyboardButtonData(other_locale[0], "other"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[0], "start"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("кис-кис", "kiskis"),
-		tgbotapi.NewInlineKeyboardButtonData(other_locale[language], "other"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Стрыкало", "strykalo"),
+			tgbotapi.NewInlineKeyboardButtonData("Нервы", "nervy"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("кис-кис", "kiskis"),
+			tgbotapi.NewInlineKeyboardButtonData(other_locale[1], "other"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[1], "start"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+back_locale[language], "start"),
-	),
-)
+}
 
-var strykalo_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Наше лето", "nashe_leto"),
-		tgbotapi.NewInlineKeyboardButtonData("Дешёвые драмы", "deshovye_dramy"),
+var strykalo_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Наше лето", "nashe_leto"),
+			tgbotapi.NewInlineKeyboardButtonData("Дешёвые драмы", "deshovye_dramy"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Кайен", "kayen"),
+			tgbotapi.NewInlineKeyboardButtonData("Фанк", "funk"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[0], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Кайен", "kayen"),
-		tgbotapi.NewInlineKeyboardButtonData("Фанк", "funk"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Наше лето", "nashe_leto"),
+			tgbotapi.NewInlineKeyboardButtonData("Дешёвые драмы", "deshovye_dramy"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Кайен", "kayen"),
+			tgbotapi.NewInlineKeyboardButtonData("Фанк", "funk"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[1], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[language], "chords"),
-	),
-)
+}
 
-var nervy_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Слишком влюблён", "slishkom_vlyublon"),
-		tgbotapi.NewInlineKeyboardButtonData("Май bye", "may_bye"),
+var nervy_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Слишком влюблён", "slishkom_vlyublon"),
+			tgbotapi.NewInlineKeyboardButtonData("Май bye", "may_bye"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Самый дорогой человек", "samy_dorogoy"),
+			tgbotapi.NewInlineKeyboardButtonData("Батареи", "batarei"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[0], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Самый дорогой человек", "samy_dorogoy"),
-		tgbotapi.NewInlineKeyboardButtonData("Батареи", "batarei"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Слишком влюблён", "slishkom_vlyublon"),
+			tgbotapi.NewInlineKeyboardButtonData("Май bye", "may_bye"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Самый дорогой человек", "samy_dorogoy"),
+			tgbotapi.NewInlineKeyboardButtonData("Батареи", "batarei"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[1], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[language], "chords"),
-	),
-)
+}
 
-var kiskis_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Молчи", "molchi"),
-		tgbotapi.NewInlineKeyboardButtonData("Мелочь", "meloch"),
+var kiskis_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Молчи", "molchi"),
+			tgbotapi.NewInlineKeyboardButtonData("Мелочь", "meloch"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Кирилл", "kirill"),
+			tgbotapi.NewInlineKeyboardButtonData("ЛБТД", "lbtd"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[0], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Кирилл", "kirill"),
-		tgbotapi.NewInlineKeyboardButtonData("ЛБТД", "lbtd"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Молчи", "molchi"),
+			tgbotapi.NewInlineKeyboardButtonData("Мелочь", "meloch"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Кирилл", "kirill"),
+			tgbotapi.NewInlineKeyboardButtonData("ЛБТД", "lbtd"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[1], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[language], "chords"),
-	),
-)
+}
 
-var other_keyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("The Middle", "middle"),
-		tgbotapi.NewInlineKeyboardButtonData("Вахтёрам", "vahteram"),
+var other_keyboard = []tgbotapi.InlineKeyboardMarkup{
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("The Middle", "middle"),
+			tgbotapi.NewInlineKeyboardButtonData("Вахтёрам", "vahteram"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Районы-кварталы", "rayoni"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[0], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Районы-кварталы", "rayoni"),
+	tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("The Middle", "middle"),
+			tgbotapi.NewInlineKeyboardButtonData("Вахтёрам", "vahteram"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Районы-кварталы", "rayoni"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[1], "chords"),
+		),
 	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("⬇ "+artists_locale[language], "chords"),
-	),
-)
+}
 
 var language_keyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
