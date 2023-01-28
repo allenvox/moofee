de3ec2e99bfe1db567603fb31c13f6abd9f37c5e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-28 13:07:28 +0700

    Add comments to the main module

--- src/main.go
+++ src/main.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	bot := initBot()
-	updates := getUpdates(bot)
-	var flag = no_flag
-	for update := range updates {
-		handleMessage(bot, update, &flag)
+	bot := initBot()              // bot initialization/authorization
+	updates := getUpdates(bot)    // get updates range from updates channel
+	var flag = no_flag            // flag for various scenarios
+	for update := range updates { // in a range of updates
+		handleMessage(bot, update, &flag) // handle each update
 	}
 }
