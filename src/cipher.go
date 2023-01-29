042c8fa15b4fd7e4e0462db81a38f6d0b5b470f2
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-29 13:16:11 +0700

    Add isEnglish and isRussian functions for letters & comment code

--- src/cipher.go
+++ src/cipher.go
@@ -4,22 +4,36 @@ import (
 	"strings"
 )
 
-func caesar(text string, shift int) string {
-	chars := []rune(strings.ToLower(text))
-	for i := 0; i < len(chars); i++ {
-		if chars[i] >= 'a' && chars[i] <= 'z' { // english
+func isRussian(letter rune) bool { // check if letter is russian
+	if letter >= 'а' && letter <= 'я' {
+		return true
+	}
+	return false
+}
+
+func isEnglish(letter rune) bool { // check if letter is english
+	if letter >= 'a' && letter <= 'z' {
+		return true
+	}
+	return false
+}
+
+func caesar(text string, shift int) string { // caesar cipher
+	chars := []rune(strings.ToLower(text)) // get an array of lowercased characters
+	for i := 0; i < len(chars); i++ {      // loop through characters
+		if isEnglish(chars[i]) { // if letter is english
 			shift = (shift%26 + 26) % 26 // only between 0 and 25
-		} else if chars[i] >= 'а' && chars[i] <= 'я' { // russian
+		} else if isRussian(chars[i]) { // russian
 			shift = (shift%33 + 33) % 33 // only between 0 and 32
 		} else {
-			continue // skip
+			continue // skip if not a letter of any alphabets
 		}
-		char := chars[i] + rune(shift)
-		if chars[i] >= 'a' && chars[i] <= 'z' {
+		char := chars[i] + rune(shift) // new char is an old char + shift
+		if isEnglish(chars[i]) {
 			if char > 'z' {
 				char -= 26
 			}
-		} else if chars[i] >= 'а' && chars[i] <= 'я' {
+		} else if isRussian(chars[i]) {
 			if char > 'я' {
 				char -= 33
 			}
