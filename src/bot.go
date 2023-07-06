3e23804909c51b81a36978fc0a2687ee187ca78b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-06 09:26:34 +0600

    Add error handling

--- src/bot.go
+++ src/bot.go
@@ -27,24 +27,30 @@ const ( // flags
 )
 
 func initBot() *tgbotapi.BotAPI {
-	tokenContent, _ := ioutil.ReadFile("token.txt")            // get token from txt file in the main folder (where bot executable is located)
-	token := string(tokenContent)                              // convert bytes gotten from txt to string
-	bot, _ := tgbotapi.NewBotAPI(token)                        // initialize bot with gotten token via API using @go-telegram-bot-api
+	tokenContent, _ := ioutil.ReadFile("token.txt") // get token from token.txt
+	token := string(tokenContent)                   // convert bytes from txt to string
+	if token == "replace_this_line_with_your_token" {
+		log.Fatal("Please change your bot token in token.txt")
+	}
+	bot, err := tgbotapi.NewBotAPI(token) // initialize bot with token via go-telegram-bot-api
+	if err != nil {
+		log.Fatal("Invalid token")
+	}
 	bot.Debug = true                                           // enable debug logging
-	log.Printf("Authorized on account @%s", bot.Self.UserName) // log that initialization completed
-	return bot                                                 // return initialized bot for the future uses
+	log.Printf("Authorized on account @%s", bot.Self.UserName) // init log
+	return bot                                                 // return init bot
 }
 
 func getUpdates(bot *tgbotapi.BotAPI) tgbotapi.UpdatesChannel {
 	u := tgbotapi.NewUpdate(0)       // last update (offset = 0)
-	u.Timeout = 60                   // set timeout to 60 seconds for last update if not getting new ones
-	updates := bot.GetUpdatesChan(u) // get updates channel from bot via API and our new update
+	u.Timeout = 60                   // set timeout to 60 seconds for last update if no updates
+	updates := bot.GetUpdatesChan(u) // get updates channel from bot via API
 	return updates
 }
 
 func send(bot *tgbotapi.BotAPI, c tgbotapi.Chattable) { // send anything sendable
-	if _, err := bot.Send(c); err != nil { // send the chattable objects (messages, images etc.)
-		panic(err) // if wasn't succesfully sent - panic & print error
+	if _, err := bot.Send(c); err != nil { // send the chattable objects (msgs, imgs etc.)
+		panic(err) // if not succesfully sent - panic & print error
 	}
 }
 
@@ -55,7 +61,7 @@ func editKeyboard(bot *tgbotapi.BotAPI, update tgbotapi.Update, keyboard tgbotap
 }
 
 func editText(bot *tgbotapi.BotAPI, update tgbotapi.Update, s string) {
-	if s != update.CallbackQuery.Message.Text { // update (edit) text only if it's different from previous
+	if s != update.CallbackQuery.Message.Text { // update msg text only if it's different from previous
 		text := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, s, *update.CallbackQuery.Message.ReplyMarkup)
 		send(bot, text)
 	}
